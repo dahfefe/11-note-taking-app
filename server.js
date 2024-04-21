d5f59eabeb2b81ec2d8493a7fa1bf17dd776f9ed
Felix <157293782+dahfefe@users.noreply.github.com>
2024-04-20 21:31:12 -0700

    copied and pasted code for server.js from miniproject to manipulate code per needs of challenge

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+// NOTE: the order this code matters!!! If you put it above line #16, it will catch all url inputs (which are strings)
+app.get('*', (req, res) =>    //   this * matches all strings that do not match line #16,21,26 (or more specifically, strings that precede this code)
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
